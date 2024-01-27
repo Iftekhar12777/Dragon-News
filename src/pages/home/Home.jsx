0f69422f9c7e1d3b630ea357bce3342b32abf4cc
Iftekhar12777 <ossi12777@gmail.com>
2024-01-27 11:55:53 +0600

    Categories adeed and a card added to news ssection

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,20 +11,46 @@ const Home = () => {
       <Header></Header>
       <BreakingNews></BreakingNews>
       <Navbar></Navbar>
-      
+
 
       <div className="mt-40 grid grid-cols-1 lg:grid-cols-4">
-           <div>
-                  <LeftSideNav></LeftSideNav>
-           </div>
+        <div>
+          <LeftSideNav></LeftSideNav>
+        </div>
 
-           <div className="md:col-span-2">
-                <h2>Coming soon!</h2>
-           </div>
+        <div className="md:col-span-2 mx-auto">
 
-           <div>
-               <RightSideNav></RightSideNav>
-           </div>
+          <div className="card px-5 bg-base-100 shadow-xl">
+
+            <div className="flex gap-2 items-center">
+
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                
+                <div className="w-10 rounded-full">
+                  <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+
+              </div>
+
+              <div>
+                <h3>Awlad Hossain</h3>
+                <p>3-4-2024</p>
+              </div>
+
+            </div>
+
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+
+            </div>
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          </div>
+
+        </div>
+
+        <div>
+          <RightSideNav></RightSideNav>
+        </div>
       </div>
     </div>
   );
--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+  const [Categories, SetCategories] = useState([])
+
+  useEffect(() => {
+    fetch('categories.json')
+    .then(res => res.json())
+    .then(data => SetCategories(data))
+  },[])
+
   return (
+    <div className="border p-4">
+      <h2 className="text-3xl font-bold mb-3">All Categories</h2>
+
     <div>
-      <h2 className="3xl">Left side nav</h2>
+          {
+            Categories.map(category => <NavLink className='block text-2xl font-medium mb-2' key={category.id}>{category.name}</NavLink>)
+          }
+    </div>
+
     </div>
   );
 };
